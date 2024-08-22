19f7474e331eb15d8524121b7de3e5202cda8306
Leonardo Fortes <leonardo56salimene@gmail.com>
2024-08-22 08:37:44 -0300

    Primeiro commit

---
+++ Dal/Conexao.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPortariaControle.Dal
+{
+    class Conexao
+    {
+        SqlConnection con = new SqlConnection();
+
+        public Conexao()
+        {
+            con.ConnectionString = @"Password=sa4321!;Persist Security Info=True;User ID=sa;Initial Catalog=rh_pim;Data Source=SERVER\SQLEXPRESS";
+        }
+        public SqlConnection conectar()
+        {
+            if (con.State == System.Data.ConnectionState.Closed)
+            {
+                con.Open();
+            }
+            return con;
+        }
+        public void desconectar()
+        {
+            if (con.State == System.Data.ConnectionState.Open)
+            {
+                con.Close();
+            }
+        }
+    }
+}
